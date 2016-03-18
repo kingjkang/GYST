6bf6509b5e8b7810c2978d1da631c5b36846aa25
kingjkang <justin.d.kang@gmail.com>
2016-03-17 23:28:00 -0500

    nav bar changes fragments

--- Gyst/app/src/main/java/com/gyst/justinkang/gyst/GYSTHome.java
+++ Gyst/app/src/main/java/com/gyst/justinkang/gyst/GYSTHome.java
@@ -1,6 +1,8 @@
 package com.gyst.justinkang.gyst;
 
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,6 +18,9 @@ import android.view.MenuItem;
 
 public class GYSTHome extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    android.app.Fragment frag = null;
+    android.app.FragmentManager fragManager = getFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,9 @@ public class GYSTHome extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        frag = new Sunflower();
+        fragManager.beginTransaction().replace(R.id.mainFrame, frag).commit();
     }
 
     @Override
@@ -79,13 +87,19 @@ public class GYSTHome extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        System.out.println(item);
+        //it prints out Events Listing and Calendar
         int id = item.getItemId();
 
         if (id == R.id.nav_events) {
-            // Handle the camera action
+            frag = new EventsListing();
         } else if (id == R.id.nav_calendar) {
-
+            frag = new Calendar();
+        } else if (id == R.id.nav_home) {
+            frag = new Sunflower();
         }
+
+        fragManager.beginTransaction().replace(R.id.mainFrame, frag).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
